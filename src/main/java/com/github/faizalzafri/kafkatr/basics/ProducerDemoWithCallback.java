08273d634ab9c77189842e8178656aa1764d3430
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2020-05-10 13:39:32 +0530

    callback producing

---
+++ src/main/java/com/github/faizalzafri/kafkatr/basics/ProducerDemoWithCallback.java
@@ -0,0 +1,35 @@
+package com.github.faizalzafri.kafkatr.basics;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+    public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "Hello Kafka from Java");
+
+        producer.send(record, (recordMetadata, e) -> {
+            if (Objects.nonNull(e)) {
+                logger.error("Error while producing: {}",e);
+            }else{
+                logger.info("Topic: {} Partition: {} Offset: {} Timestamp: {}",recordMetadata.topic(),recordMetadata.partition(),recordMetadata.offset(),recordMetadata.timestamp());
+            }
+        });
+
+        producer.flush();
+    }
+}
