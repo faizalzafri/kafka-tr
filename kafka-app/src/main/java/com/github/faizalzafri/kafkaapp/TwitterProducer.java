5bc1e5500f00f052a2969f015c0b640e6ffeecb7
faizalzafri <34762675+faizalzafri@users.noreply.github.com>
2020-05-12 12:35:49 +0530

    safe idempotent producer L115

--- kafka-app/src/main/java/com/github/faizalzafri/kafkaapp/TwitterProducer.java
+++ kafka-app/src/main/java/com/github/faizalzafri/kafkaapp/TwitterProducer.java
@@ -1,4 +1,4 @@
-package app;
+package com.github.faizalzafri.kafkaapp;
 
 import com.google.common.collect.Lists;
 import com.twitter.hbc.ClientBuilder;
@@ -111,6 +111,12 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create safe producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG,"all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG,Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
 
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
         return producer;
